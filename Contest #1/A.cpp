51100df75c0de1d938c14e0f1ce11a822b8ba0e7
Omar Ashraf <110625103+Omar-26@users.noreply.github.com>
2023-06-10 03:08:15 +0300

    Delete Contest #1 directory

--- Contest #1/A.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main()
-{
-    float x, p;
-    cin >> x >> p;
-    cout << (p) / (1 - (x / 100));
-}   
--- Contest #1/C.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main()
-{
-    char a;
-    cin >> a;
-    cout << ((a >= 97 && a < 122) ? char(a + 1) : 'a');
-}
